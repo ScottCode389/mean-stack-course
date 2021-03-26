f4ac6c7610ae37fd9764027223ce7d327bc6c8b6
J Scott <jbscott@gmail.com>
2021-03-26 08:48:26 -0400

    start setting up the components using CLI and Angular/Material

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    { title: 'First Post', content: 'This is the first post content' },
+    { title: 'Second Post', content: 'This is the Second post content' },
+    { title: 'Third Post', content: 'This is the third post content' },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
