739c60f32bfac20c596680c1956c854b5805ea66
J Scott <jbscott@gmail.com>
2021-04-09 10:39:53 -0400

    Finished setting up the backend server connecting to Mongoose and MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Post = require('./models/post');
 const app = express();
+
+mongoose.connect("mongodb+srv://janet:uSIs38Iored8Pk1G@cluster0.jngke.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,29 +26,33 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save().then(result => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
+  });
+
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fadf124211',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'lkjsadkrej',
-      title: 'Second server-side post',
-      content: 'This one is also coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Post deleted!' });
   });
 });
 
